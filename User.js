13abc08f0eb53e4e8662abee6dee5dedb2e7a079
ryo-momo <ryotaro@momotarou-f.com>
2024-01-20 11:27:07 +0900

    feat:implement getCurrentMinorState()

--- User.js
+++ User.js
@@ -6,7 +6,7 @@ class User {
         this.major_state_id = user_property.user_state.major_state_id;
         this.minor_state_id = user_property.user_state.minor_state_id;
         this.current_action_id = user_property.user_state.current_action_id;
-        this.current_survey_id = user_property.user_state.current_survey_id
+        this.current_survey_id = user_property.user_state.current_survey_id;
         this.current_step_id = user_property.user_state.current_step_id;
         this.current_question = user_property.user_state.current_question;
     }
@@ -19,8 +19,15 @@ class User {
         }
     }
 
+    ///returns the user's current minor state object
+    getCurrentMinorState(){
+        user_states = JSON.parse(fs.readFileSync('./user_states.json', 'utf8'));
+        minor_states = user_states.major_states.find(major_state => major_state.state_id === this.major_state_id).mimor_states
+        return minor_states.find(minor_state => minor_state.state_id === this.minor_state_id);
+    }
+
     getCurrentStep(){
-        actions = JSON.parse(fs.readFileSync('./user_states.json', 'utf8'));
+        actions = JSON.parse(fs.readFileSync('./user_states.json', 'utf8')).actions;
         steps = actions.find(action => action.action_id === this.current_action_id).steps;
         return steps.find(step => step.step_id === this.current_step_id);
     }
