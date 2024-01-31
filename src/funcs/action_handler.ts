454be3968b4ee4f8f6518044b9fa401067fcd4af
ryo-momo <ryotaro@momotarou-f.com>
2024-01-31 17:57:31 +0900

    refactor action_handler.ts

--- src/funcs/action_handler.ts
+++ src/funcs/action_handler.ts
@@ -1,24 +1,26 @@
 import { User } from '../classes/User';
 import { Action } from '../data/user_states';
 
-export function actionInvoker(user: User, text: string, action?: Action | null): User {
-    //Invoke the action
-    if (action) {
-        console.log('Invoking new triggered action');
-        user.current_action_id = action.action_id;
-        if (action.handler) {
-            user = action.handler(user, text);
-        } else {
-            throw new Error('Handler not found in action');
-        }
+const ERROR_MESSAGES = {
+    HANDLER_NOT_FOUND: 'Handler not found in action',
+};
+
+function invokeAction(user: User, text: string, action: Action): User {
+    console.log('Invoking action');
+    user.current_action_id = action.action_id;
+    if (action.handler) {
+        user = action.handler(user, text);
     } else {
-        console.log("Invoking user's current action");
-        const current_action = user.getCurrentAction();
-        if (current_action.handler) {
-            user = current_action.handler(user, text);
-        } else {
-            throw new Error('Handler not found in action');
-        }
+        throw new Error(ERROR_MESSAGES.HANDLER_NOT_FOUND);
     }
     return user;
+}
+
+export function actionInvoker(user: User, text: string, action?: Action | null): User {
+    if (action) {
+        return invokeAction(user, text, action);
+    } else {
+        const current_action = user.getCurrentAction();
+        return invokeAction(user, text, current_action);
+    }
 }
